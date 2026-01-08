6e7afa4a04b2e9788a3abbdc2537f6383a599b4f
Jair Ice Cold <jairicecold@gmail.com>
2026-01-07 20:23:57 -0500

    PHP-6-7

---
+++ bases_php/php/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UG</title>
+</head>
+
+<body>
+    <h1>Binevenido</h1>
+    <?php
+    $usuario = "Alejandro";
+    echo "<p>Hola $usuario </p>";
+    ?>
+    <p>Hola <?= $usuario ?></p>
+</body>
+
+</html>
